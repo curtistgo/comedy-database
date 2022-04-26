7fb48f3ceee10cb9c90041606dca7e5ff86485bb
Curtis <curtistgo@gmail.com>
2022-04-26 15:03:06 -0400

    Removed improper html tags from the Paragragh component.

--- src/components/Paragraph.jsx
+++ src/components/Paragraph.jsx
@@ -3,24 +3,22 @@ import React from "react";
 export default class Paragraph extends React.Component {
   render() {
     return (
-      <html>
-        <div className="container-fluid paraFormat">
-          <p>Hi Folks!</p>
+      <div className="container-fluid paraFormat">
+        <p>Hi Folks!</p>
 
-          <p>
-            On this site I want to appreciate all comedic writers and actors.
-            The database on this page acknowledges some of the greatest comedies
-            beginning in the 1970s.
-          </p>
+        <p>
+          On this site I want to appreciate all comedic writers and actors. The
+          database on this page acknowledges some of the greatest comedies
+          beginning in the 1970s.
+        </p>
 
-          <p>
-            Comedic movies deserve equal respect as dramatic movies. After all,
-            comedy has transformative powers. Comedies have been known to
-            transform people in miserable moods into lighter more affable
-            people. Comedy is happiness.
-          </p>
-        </div>
-      </html>
+        <p>
+          Comedic movies deserve equal respect as dramatic movies. After all,
+          comedy has transformative powers. Comedies have been known to
+          transform people in miserable moods into lighter more affable people.
+          Comedy is happiness.
+        </p>
+      </div>
     );
   }
 }
